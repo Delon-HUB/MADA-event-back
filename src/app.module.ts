ebc9252cd7145c9bf9a2dfd8e0ff15b012a65e89
Delon <nicolas123delon@gmail.com>
2025-10-20 17:31:13 +0300

    feat(init): configure connection to mongoDB with moongose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost/MADA-event:270017',
+    ),
+  ],
 })
 export class AppModule {}
