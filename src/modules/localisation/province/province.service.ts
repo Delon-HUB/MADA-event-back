30f85da4415cbdc31509d039790d6535a73b3709
Delon <nicolas123delon@gmail.com>
2025-10-23 15:04:59 +0300

    feat(localisation): create province

--- src/modules/localisation/province/province.service.ts
+++ src/modules/localisation/province/province.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { ProvinceEntity } from './entities/province.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ICreateProvinceDto } from './dto/create-province.dto';
 
 @Injectable()
-export class ProvinceService {}
+export class ProvinceService {
+  constructor(
+    @InjectModel(ProvinceEntity.name)
+    private readonly provinceModel: Model<ProvinceEntity>,
+  ) {}
+
+  async create(province: ICreateProvinceDto): Promise<ICreateProvinceDto> {
+    const isAlreadyCreated =
+      (await this.findByName(province.provinceName)) != null;
+    if (isAlreadyCreated)
+      throw new HttpException('PROVINCE_ALREADY_EXIST', 400);
+    const newProvince = (
+      await new this.provinceModel(province).save()
+    ).toObject();
+    return {
+      ...newProvince,
+      _id: newProvince._id.toString(),
+    };
+  }
+
+  async findByName(provinceName: string): Promise<ProvinceEntity | null> {
+    return this.provinceModel.findOne({ provinceName: provinceName }).exec();
+  }
+}
