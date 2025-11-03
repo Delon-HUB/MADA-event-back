7d900c36b4173ac403768f537643dc66f26f2112
Delon <nicolas123delon@gmail.com>
2025-11-03 23:19:08 +0300

    feat: upload file

--- src/modules/event/event.controller.ts
+++ src/modules/event/event.controller.ts
@@ -1,14 +1,41 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { EventService } from './event.service';
+import { FileInterceptor, MulterModule } from '@nestjs/platform-express';
 import { ICreateEventDto } from './dto/create-event.dto';
+import { diskStorage } from 'multer';
 
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
   @Post()
-  create(@Body() createEventDto: ICreateEventDto) {
-    return this.eventService.create(createEventDto);
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './public/event',
+        filename: (req, file, cb) => {
+          const filename =
+            'event-' +
+            new Date(Date.now()).toISOString() +
+            `.${file.mimetype.split('/')[1]}`;
+          cb(null, filename);
+        },
+      }),
+    }),
+  )
+  async create(
+    @UploadedFile() img: Express.Multer.File,
+    @Body() data: ICreateEventDto,
+  ) {
+    console.log(img);
+    console.log(data.title);
   }
 
   @Get()
