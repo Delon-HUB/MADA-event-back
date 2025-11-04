255ea15043b89b4b5c81600132d7afe2ca609a0c
Delon <nicolas123delon@gmail.com>
2025-11-04 10:04:53 +0300

    feat: create event gateway using socket.io

--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventService } from './event.service';
 import { EventController } from './event.controller';
+import { EventGateway } from './event.gateway';
 
 @Module({
   controllers: [EventController],
-  providers: [EventService],
+  providers: [EventService, EventGateway],
 })
 export class EventModule {}
