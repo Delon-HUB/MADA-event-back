c761edf83771d34d7d771b7756a43f2d8453426f
Delon <nicolas123delon@gmail.com>
2025-11-04 11:47:45 +0300

    feat: update module

--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { EventService } from './event.service';
 import { EventController } from './event.controller';
 import { EventGateway } from './event.gateway';
+import { EventEntity, EventSchema } from './entities/event.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: EventEntity.name, schema: EventSchema },
+    ]),
+  ],
   controllers: [EventController],
   providers: [EventService, EventGateway],
 })
