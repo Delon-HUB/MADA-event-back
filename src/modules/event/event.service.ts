6945d8472f4e0aa7b176dcb3cff6ad1fd9ce31c4
Delon <nicolas123delon@gmail.com>
2025-11-04 11:48:07 +0300

    feat: create event service

--- src/modules/event/event.service.ts
+++ src/modules/event/event.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { ICreateEventDto } from './dto/create-event.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { EventEntity } from './entities/event.entity';
 
 @Injectable()
 export class EventService {
-  create(createEventDto: ICreateEventDto) {
-    return 'This action adds a new event';
+  constructor(
+    @InjectModel(EventEntity.name)
+    private readonly eventModel: Model<EventEntity>,
+  ) {}
+
+  async create(createEventDto: ICreateEventDto): Promise<ICreateEventDto> {
+    const newEvent = (await this.eventModel.create(createEventDto)).toObject();
+    return {
+      ...newEvent,
+      _id: newEvent._id.toString(),
+    };
   }
 
   findAll() {
