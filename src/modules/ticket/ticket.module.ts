d72ebce72cec3aeb4e97ab1216b06c4f7cd3b4f7
Delon <nicolas123delon@gmail.com>
2025-11-06 21:25:44 +0300

    feat: find ticket by userId

--- src/modules/ticket/ticket.module.ts
+++ src/modules/ticket/ticket.module.ts
@@ -3,11 +3,15 @@ import { TicketService } from './ticket.service';
 import { TicketController } from './ticket.controller';
 import { TicketEntity, TicketSchema } from './entities/ticket.entity';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { EventSchema } from '../event/entities/event.entity';
 
 @Module({
   imports: [
+    JwtModule,
     MongooseModule.forFeature([
       { name: TicketEntity.name, schema: TicketSchema },
+      { name: 'events', schema: EventSchema },
     ]),
   ],
   controllers: [TicketController],
--- src/modules/ticket/ticket.service.ts
+++ src/modules/ticket/ticket.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ICreateTicketDto } from './dto/create-ticket.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { TicketEntity } from './entities/ticket.entity';
 
 @Injectable()
@@ -26,7 +26,19 @@ export class TicketService {
     return created;
   }
 
-  findAll() {
-    return `This action returns all ticket`;
+  async findByUserId(userId: string): Promise<ICreateTicketDto[]> {
+    const ticketEntites = await this.ticketModel
+      .find({ userId: userId })
+      .populate({ path: 'eventId' })
+      .exec();
+    const tickets: ICreateTicketDto[] = ticketEntites.map((t) => {
+      const ticket = t.toObject();
+      return {
+        ...ticket,
+        _id: ticket._id.toString(),
+        userId: ticket.userId.toString(),
+      };
+    });
+    return tickets;
   }
 }
