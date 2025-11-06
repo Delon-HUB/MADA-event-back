91295fb76c6bdf5a481827ad2c6cd4a1ae23588d
Delon <nicolas123delon@gmail.com>
2025-11-06 15:38:16 +0300

    feat(payment): create entity and dto

---
+++ src/modules/payment/entities/payment.entity.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { type ObjectId, SchemaTypes } from 'mongoose';
+
+@Schema({ timestamps: true, collection: 'payments' })
+export class PaymentEntity {
+  @Prop({ required: true, type: SchemaTypes.ObjectId, ref: 'events' })
+  eventId: ObjectId;
+
+  @Prop({ required: true, type: SchemaTypes.ObjectId, ref: 'users' })
+  userId: ObjectId;
+
+  @Prop({ required: true })
+  amount: number;
+
+  @Prop({ required: true })
+  method: string;
+
+  @Prop({ required: true })
+  phoneNumber: string;
+
+  @Prop()
+  status?: string;
+
+  @Prop({ required: true, default: new Date() })
+  createdAt: Date;
+
+  @Prop({ required: true, default: new Date() })
+  updatedAt: Date;
+}
+
+export const PaymentSchema = SchemaFactory.createForClass(PaymentEntity);
