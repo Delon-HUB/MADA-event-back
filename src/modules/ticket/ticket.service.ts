f85ca774e3a16dd6c3fe7f6e21e38861bb745391
Delon <nicolas123delon@gmail.com>
2025-11-06 21:25:44 +0300

    feat: find ticket by userId

--- src/modules/ticket/ticket.service.ts
+++ src/modules/ticket/ticket.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ICreateTicketDto } from './dto/create-ticket.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { TicketEntity } from './entities/ticket.entity';
 
 @Injectable()
