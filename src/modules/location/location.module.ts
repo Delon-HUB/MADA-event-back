d929bf221e8e91113b1d4df213ba856847754abd
Delon <nicolas123delon@gmail.com>
2026-06-19 11:07:32 +0300

    feat: enhance LocationService to include districts, communes, and quarters in findAll method

--- src/modules/location/location.module.ts
+++ src/modules/location/location.module.ts
@@ -4,10 +4,28 @@ import { LocationController } from './location.controller';
 import { LocationService } from './location.service';
 import { RegionService } from './region/region.service';
 import { RegionModule } from './region/region.module';
+import { CommuneService } from './commune/commune.service';
+import { DistrictService } from './district/district.service';
+import { QuarterService } from './quarter/quarter.service';
+import { DistrictModule } from './district/district.module';
+import { CommuneModule } from './commune/commune.module';
+import { QuarterModule } from './quarter/region.module';
 
 @Module({
-  imports: [MongooseModule, RegionModule],
+  imports: [
+    MongooseModule,
+    RegionModule,
+    DistrictModule,
+    CommuneModule,
+    QuarterModule,
+  ],
   controllers: [LocationController],
-  providers: [LocationService, RegionService],
+  providers: [
+    LocationService,
+    RegionService,
+    DistrictService,
+    CommuneService,
+    QuarterService,
+  ],
 })
 export class LocationModule {}
